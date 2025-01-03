fb64546900812e31db2d190ceaba47b10cdaa0cd
Ana Boldeanu <ana_maria.boldeanu@stud.acs.upb.ro>
2025-01-03 16:29:00 +0200

    VR Lab 3 Setup

---
+++ IRVA_VR/Assets/L3_VR_SteamVR_Advanced/Scripts/Input/SteamVRInputActionsTesting.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+namespace L3_VR_SteamVR_Advanced.Scripts.Input
+{
+    public class SteamVRInputActionsTesting : MonoBehaviour
+    {
+        // TODO 1 : Setup input for the already-defined `GrabPinch` action.
+        //          Write a message in the console which signifies this input is correctly read.
+        //          Use either the polling method or an event-based mechanism.
+        
+        // TODO 2 : Setup input for the `TouchTrigger` action (you'll have to first create it & bind it accordingly)
+        //          Write a message in the console which signifies this input is correctly read.
+        //          Use either the polling method or an event-based mechanism.
+    }
+}
